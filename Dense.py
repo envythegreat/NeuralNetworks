b4bb639dafc5fbd71cf56c81a912d70208038787
Mohamed Habib Alouani <mhabib.alouani@gmail.com>
2021-12-07 12:47:18 +0100

    Softmax Activation Function

--- Dense.py
+++ Dense.py
@@ -36,6 +36,14 @@ class Activation_ReLU:
     # Zero gradient where input values were negative
     self.dinputs[self.inputs <= 0] = 0
 
+class Activation_Softmax:
+  def forward(self, inputs):
+    # Get unnormalized probabilities
+    expValues = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+    # Normalize them for each sample
+    probabilities = expValues / np.sum(expValues, axis=1, keepdims=True)
+    self.output = probabilities
+
 
 
 
