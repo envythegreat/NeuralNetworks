7a1ab3f730827540687eb79653f1cb94213c6cef
Mohamed Habib Alouani <mhabib.alouani@gmail.com>
2021-12-07 12:45:58 +0100

    Relu Activation Function

--- Dense.py
+++ Dense.py
@@ -24,6 +24,18 @@ class Layer_Dense:
   
 
 
+class Activation_ReLU:
+  def forward(self, inputs):
+    # Calculate output values from inputs
+    self.output = np.maximum(0, inputs)
+  # Backward pass
+  def backward(self, dvalues):
+    # Since we need to modify the original variable,
+    # let's make a copy of the values first
+    self.dinputs = dvalues.copy()
+    # Zero gradient where input values were negative
+    self.dinputs[self.inputs <= 0] = 0
+
 
 
 
