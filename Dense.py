4bf280b99088d53e756326d4828e8fae472f76fb
Mohamed Habib Alouani <mhabib.alouani@gmail.com>
2021-12-26 16:58:17 +0100

    Linear Regression Activation function

--- Dense.py
+++ Dense.py
@@ -370,6 +370,14 @@ class OptimizerRMSprop:
     self.iterations += 1
 
 
+class Activation_linear:
+
+  def forward(self, inputs):
+    self.inputs = inputs
+    self.output = inputs
+  
+  def backward(self, dvalues):
+    self.dinputs = dvalues.copy()
 
 
 
