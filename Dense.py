0f5e8109c59eeb33911cb48089ce30ad8ddd50b0
Mohamed Habib Alouani <mhabib.alouani@gmail.com>
2022-01-01 16:15:31 +0100

    start testing the model

--- Dense.py
+++ Dense.py
@@ -834,18 +834,18 @@ class AccuracyCategorical(Accuracy):
 
 # model.finalize()
 # model.train(X, y, epochs=10000, printEvery=100)
+ 
 
-
-X, y = spiral_data(samples=100, classes=3)
-X_test, y_test = spiral_data(samples=10, classes=3)
+X, y = spiral_data(samples=200, classes=3)
+X_test, y_test = spiral_data(samples=20, classes=3)
 # Instantiate the model
 model = Model()
 # Add layers
-model.add(Layer_Dense(2, 64, weight_regularizer_l2=5e-4,
+model.add(Layer_Dense(2, 512, weight_regularizer_l2=5e-4,
 bias_regularizer_l2=5e-4))
 model.add(Activation_ReLU())
 model.add(LayerDropout(0.1))
-model.add(Layer_Dense(64, 3))
+model.add(Layer_Dense(512, 3))
 model.add(Activation_Softmax())
 # Set loss, optimizer and accuracy objects
 model.set(
